d91f2454c6d71b08bec5ddbf601c0bfdc71b4fe9
Moatasem-Elsayed <eng.moatasem.8@gmail.com>
2022-06-21 15:01:09 +0200

    module_param

--- test/hello.c
+++ test/hello.c
@@ -1,1 +1,31 @@
 #include <linux/module.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Moatasem ");
+MODULE_DESCRIPTION("A hello world Psuedo device driver");
+
+int cnt = 0;
+module_param(cnt, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(cnt, "An integer");
+
+static int __init hellodriver_init(void)
+{
+    int i = 0;
+    for (i = 0; i < cnt; i++)
+        printk("hello world\n");
+    return 0;
+}
+static void __exit hellodriver_exit(void)
+{
+    int i;
+    for (i = 0; i < cnt; i++)
+        printk("good bye\n");
+}
+module_init(hellodriver_init);
+module_exit(hellodriver_exit);
+// lsmod
+// insmod # modprobe
+// rmmod
+// modinf
